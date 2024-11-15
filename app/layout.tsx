29bef0003badaa77709cde411789547b79d00019
hrithik210 <hrithiknayak7829@gmail.com>
2024-11-15 23:13:43 +0530

    added some stypes

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-teal-400`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#212121]`}
       >
         {children}
       </body>
--- components/TokenLaunchpad.tsx
+++ components/TokenLaunchpad.tsx
@@ -2,14 +2,14 @@
 
 export function TokenLaunchpad() {
   return  <div  className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl mb-5 font-extrabold text-gray-900">Solana Token Launchpad</h1>
-      <div className="flex flex-col max-w-lg mx-auto ">
-        <input className='bg-slate-300 shadow-md rounded-md text-center' type='text' placeholder='Name'></input> <br />
-        <input className='bg-slate-300 shadow-md rounded-md text-center' type='text' placeholder='Symbol'></input> <br />
-        <input className='bg-slate-300 shadow-md rounded-md text-center' type={"file"} placeholder='Image URL'></input> <br />
-        <input className='bg-slate-300 shadow-md rounded-md text-center' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+      <h1 className="text-4xl mb-5 font-extrabold text-white">Solana Token Launchpad</h1>
+      <div className="flex flex-col max-w-lg mx-auto shadow-md ">
+        <input className='bg-slate-200 shadow-md rounded-md text-center px-5 py-3 text-black' type='text' placeholder='Name'></input> <br />
+        <input className='bg-slate-200 shadow-md rounded-md text-center px-5 py-3 text-black' type='text' placeholder='Symbol'></input> <br />
+        <input className='bg-slate-200 shadow-md rounded-md text-center px-5 py-3 text-black' type={"file"} placeholder='Image URL'></input> <br />
+        <input className='bg-slate-200 shadow-md rounded-md text-center px-5 py-3 text-black' type='text' placeholder='Initial Supply'></input> <br />
       </div>
+      <button className='bg-teal-500 py-2 px-3 shadow-md rounded-lg text-black text-lg font-semibold'>Create token</button>
       
   </div>
 }
